a988f535baba1f2141bbb6457a2498171fb9ecf5
Shanna J. Smith <shannasmith@Shannas-Air.phub.net.cable.rogers.com>
2021-11-03 13:01:58 -0400

    commented out incomplete updatespots function and wrote an intro in the README.md

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -24,19 +24,21 @@ export default function useApplicationData() {
 
   const setDay = day => setState({ ...state, day });
 
-  const updateSpots = (state) => {
-    const day = state.days.find(day => day.name === state.day)
-    let spots = 0;
-    for (const id of day.appointments) {
-      const appointment = state.appointments[id]
-      if (!appointment.interview) {
-      spots++
-    }
-  }
-    day.spots = spots
-    const newDay = {...day, spots}
-    return state.days.map(day => day.name === state.day ? newDay : day)
-  };
+  // const updateSpots = (state) => {
+  // console.log("state.days", )
+
+  //   const day = state.days.find(day => day.name === state.day)
+  //   let spots = 0;
+  //   for (const id of day.appointments) {
+  //     const appointment = state.appointments[id]
+  //     if (!appointment.interview) {
+  //       spots++
+  //   }
+  // }
+  //   day.spots = spots
+  //   const newDay = {...day, spots}
+  //   return state.days.map(day => day.name === state.day ? newDay : day)
+  // };
 
   const bookInterview = (id, interview) => {
     const something = state.appointments[id].interview !== null;
@@ -48,14 +50,13 @@ export default function useApplicationData() {
       ...state.appointments,
       [id]: appointment
     };
-const days = updateSpots(id, state, something ? 0 : -1)
+//const days = updateSpots(id, state, something ? 0 : -1)
     return axios
       .put(`http://localhost:8001/api/appointments/${id}`, {interview})
       .then((res) => {
       setState({
         ...state,
-        appointments, 
-        days
+        appointments
         });
     })
   }
@@ -69,14 +70,13 @@ const days = updateSpots(id, state, something ? 0 : -1)
       ...state.appointments,
       [id]: appointment
     };
-    const days = updateSpots(id, state, 1)
+    //const days = updateSpots(id, state, 1)
     return axios
       .delete(`http://localhost:8001/api/appointments/${id}`)
       .then((res) => {
         setState({
           ...state,
-          appointments,
-          days
+          appointments
         });
     })
   }
